97c2f8f38b62c4bab099c8119b6c1c1035293f80
Li Zul <52091744+LiZool@users.noreply.github.com>
2026-04-12 14:43:55 +0800

    commit
    made changes to welcome page

--- educational-app/frontend/src/pages/Welcome.jsx
+++ educational-app/frontend/src/pages/Welcome.jsx
@@ -4,54 +4,101 @@ import { motion } from "framer-motion";
 /* images */
 import character from "../assets/images/png-bzcku_monkey.png"
 import border from "../assets/images/border/border3v2.png"
-import border2 from "../assets/images/border/border2v2.png"
+import border_2 from "../assets/images/border/border3v2_2.png"
 
 export default function Welcome() {
   return (
     <div className="min-h-screen bg-green-400 flex flex-col items-center justify-center text-center px-6">
-      {/* Text */}
-      <h1 className="text-2xl font-bold mb-2">
-        Kids Education
-      </h1>
-      <p className="text-lg mb-6">
-        Play, learn and grow
-      </p>
+      {/* Title text */}
+      <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="text-5xl md:text-6xl font-extrabold mb-2 text-white"
+        >
+          <span className="text-yellow-300">Kids</span>{" "}
+          <span className="text-white">Education</span>
+          
+          
+        </motion.h1>
 
-        <div className="relative w-full max-w-[1000px] aspect-[3/1] mt-20">
-
-        {/* Character (BACK) */}
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.3 }}
+          className="text-base md:text-lg text-white/90 mb-6"
+        >
+          <h1>
+            <span className="font-fredoka text-2xl md:text-3xl font-bold text-yellow-300">Let’s</span>{" "}
+            <span className="font-fredoka text-white">Play & Learn!</span>
+          </h1>
+      </motion.p>
+      
+      {/* Welcom Content */}
+      <div className="relative w-full max-w-[700px] mt-20">
+        {/* Character */}
         <motion.img
           src={character}
+          className="
+            absolute -top-20 left-[55%] md:left-[50%] 
+            -translate-x-1/2
+            w-[230px] z-0"
+        />
+
+        {/* Wrapper - Border */}
+        <motion.div 
           animate={{ y: [0, -10, 0] }}
           transition={{ repeat: Infinity, duration: 1 }}
-          className="absolute -top-20 left-[42%] -translate-x-1/2 w-[180px] z-0"
-        />
+          className="relative mx-auto mt-20  z-10 ">
+            
+          {/* Desktop-Tablet-Larger screens */}
+          <img
+            src={border}
+            animate={{ y: [0, -10, 0] }}
+            transition={{ repeat: Infinity, duration: 1 }}
+            className="hidden md:block w-full h-auto object-contain"
+          />
 
-        {/* Border (MIDDLE) */}
-        <img
-          src={border}
-          className="absolute left-1/2 top-10 -translate-x-1/2 w-[75%] h-auto object-contain pointer-events-none z-10"
-        />
+          {/* Mobile - Smaller Screens */}
+          <img
+            src={border_2}
+            className="block md:hidden w-[120%] h-auto object-contain mx-auto"
+          />
 
-        {/* Buttons (FRONT) */}
-        <div className="absolute inset-0 flex items-center justify-center gap-4 -translate-y-1 translate-x-5 z-20">
-          <button> Start </button>
-          <button> Progress </button>
-          <button> Achivement </button>
-        </div>
+          {/* Buttons */}
+          <div className="absolute inset-0 flex flex-col md:flex-row items-center justify-center gap-6 lg:gap-10 -translate-y-3 translate-x-2">
+            <motion.button 
+              whileHover={{ scale: 1.2 }} 
+              transition={{ type: "spring", stiffness: 300 }}
+              className="text-xl md:text-2xl lg:text-3xl text-white hover:text-yellow-300 transition-colors duration-200">
+                <h1> Start </h1>
+            </motion.button>
+
+            <motion.button 
+              whileHover={{ scale: 1.2 }} 
+              transition={{ type: "spring", stiffness: 300 }}
+              className="text-xl md:text-2xl lg:text-3xl text-white hover:text-yellow-300 transition-colors duration-200">
+                Progress
+            </motion.button>
+
+            <motion.button 
+              whileHover={{ scale: 1.2 }} 
+              transition={{ type: "spring", stiffness: 300 }}
+              className="text-xl md:text-2xl lg:text-3xl text-white hover:text-yellow-300 transition-colors duration-200">
+                Achievement
+            </motion.button>
+          </div>
+        </motion.div>
     </div>
-
-
-      {/* Main Button */}
-      <motion.button
+    
+    {/* Main Button */}
+    <motion.button
         whileTap={{ scale: 0.9 }}
         whileHover={{ scale: 1.05 }}
         className="bg-yellow-400 text-black text-xl font-bold py-4 px-8 rounded-full shadow-lg"
       >
         Let’s Start!
       </motion.button>
-
-      
 
       {/* Parent option */}
       <button className="mt-6 text-sm text-gray-600 underline">
